56b401a38aeb93ea87356f57fdb7067ac7c2fc0a
kgridou <32600911+khalilou88@users.noreply.github.com>
2025-09-10 23:15:01 +0200

    build: wip

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,18 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>Welcome to {{ title() }}!</h1>
 
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+    <article class="prose">
+      <h1>Your content here</h1>
+      <p>Beautiful typography without any JavaScript.</p>
+    </article>
+
+    <!-- Size variants must be used with base prose class -->
+    <article class="prose prose-lg">
+      <h1>Larger typography</h1>
+    </article>
+
     <router-outlet />
   `,
   styles: [],
